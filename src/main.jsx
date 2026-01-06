7a05b801d40e875078e1b7a9436e09f2f1f3965a
Vinnie <davsalzid@gmail.com>
2026-01-06 19:21:00 +0700

    fix typo

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,21 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
-import Navbar from './components/Navbar.jsx';
-import Footer from './components/footer.jsx';
-import PreLoader from './components/PreLoader.jsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
+import PreLoader from "./components/PreLoader.jsx";
 
-import 'remixicon/fonts/remixicon.css';
+import "remixicon/fonts/remixicon.css";
 
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
 
-import 'animate.css';
+import "animate.css";
 // ..
 AOS.init();
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <PreLoader />
     <div className="container mx-auto px-4">
@@ -23,5 +23,5 @@ createRoot(document.getElementById('root')).render(
       <App />
       <Footer />
     </div>
-  </StrictMode>,
-)
+  </StrictMode>
+);
